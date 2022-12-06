35817fd1341aa79fc82e9f08447608f9261558dc
romingoon <96216099+romingoon@users.noreply.github.com>
2022-12-06 18:41:14 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,6 +1,1 @@
-function solution(num) {
-    var answer = '';
-    if(num===0 | num%2===0) answer='Even';
-    else answer ='Odd';
-    return answer;
-}
+const solution = (num) => num % 2 === 0 ? "Even" : "Odd";
