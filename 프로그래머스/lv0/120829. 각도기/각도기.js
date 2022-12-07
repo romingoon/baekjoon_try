c2b44147cc1f952eb8cac38047b3fbf1bc688e22
romingoon <96216099+romingoon@users.noreply.github.com>
2022-12-07 16:27:38 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,11 +1,3 @@
 const solution = (angle) => {
-  if (0 < angle && angle < 90) {
-    return 1;
-  } else if (angle === 90) {
-    return 2;
-  } else if (90 < angle && angle < 180) {
-    return 3;
-  } else {
-    return 4;
-  }
+  return angle < 90 ? 1 : angle === 90 ? 2 : angle < 180 ? 3 : 4;
 };
