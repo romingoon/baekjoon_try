3f1a6ff5466ea8be6b08289d7dda8a768cc66ef6
romingoon <96216099+romingoon@users.noreply.github.com>
2022-12-08 13:57:09 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,9 +1,3 @@
 const solution = (array, n) => {
-  let cnt = 0;
-  for (item of array) {
-    if (item === n) {
-      cnt += 1;
-    }
-  }
-  return cnt;
+  return array.filter((v) => v === n).length;
 };
