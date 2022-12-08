b4e398169b551ec66cc5b3f4cbefd08701dee4cb
romingoon <96216099+romingoon@users.noreply.github.com>
2022-12-08 14:12:43 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -2,6 +2,6 @@ const solution = (num_list) => {
     
     return [num_list.filter((item) => item % 2 === 0).length, 
             num_list.filter((item) => item % 2 !== 0).length
-           ]
+           ];
 
 };
