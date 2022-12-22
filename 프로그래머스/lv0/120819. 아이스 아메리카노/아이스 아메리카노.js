54ca87980cdda1bc846f4616aecdd2abbcff5dbd
romingoon <96216099+romingoon@users.noreply.github.com>
2022-12-22 14:59:17 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,6 +1,3 @@
 const solution = (money) =>{
-    const res = [];
-    res.push(parseInt(money / 5500));
-    res.push(parseInt(money % 5500));
-    return res;
+    return [parseInt(money / 5500), money%5500];
 }
