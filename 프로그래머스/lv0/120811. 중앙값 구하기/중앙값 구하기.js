163d6079f11d87a18d23b1c42b1551e0a3bdf541
romingoon <96216099+romingoon@users.noreply.github.com>
2023-01-07 17:25:29 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,5 +1,3 @@
-function solution(array) {
-    array.sort((a,b)=>a-b);
-    const val = parseInt(array.length/2);
-    return array[val];
+const solution = (array) =>{
+    return array.sort((a,b)=>a-b)[parseInt(array.length/2)];
 }
